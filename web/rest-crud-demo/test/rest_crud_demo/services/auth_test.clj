831dc96a1ff808c0f087d7a8154c9bcfe89a541f
Niklas Collin <niklas.collin@thoughtworks.com>
2021-04-09 10:40:45 +0300

    Format auth-test

--- web/rest-crud-demo/test/rest_crud_demo/services/auth_test.clj
+++ web/rest-crud-demo/test/rest_crud_demo/services/auth_test.clj
@@ -22,15 +22,15 @@
   (testing "should encode user"
     ;TODO: I hate the call to current time from within encode function. Maybe inject it?
     (let [token (+ (quot (System/currentTimeMillis) 1000) (* 60 60 24 7))]
-      (is (= (str  "10:user:admin:1:" token)
+      (is (= (str "10:user:admin:1:" token)
              (auth/encode {:id       10
                            :username "user"
                            :role     "admin"})))))
-  
+
   (testing "should sign message with password"
     (is (= "f109dbd1c8e124e9e0bbba2116bb167da7d607672d12a58cdd6ea15e637585ce"
            (auth/sign "a message" "a password"))))
-  
+
   (testing "should allow access to resource for admin user"
     (let [pw-hash       "f9480456ebc3a3259592d1affa832643c3f7e29c3cf5f8afcfeaa62fb84f1d85"
           expected-user (with-exp {:id "3", :username "user", :role "admin", :auth "1"})
@@ -39,7 +39,7 @@
 
       (is (true? (:fake-called? response)))
       (is (= expected-user (:identity response)))))
-  
+
   (testing "should allow access to resource for power user"
     (let [pw-hash       "f9480456ebc3a3259592d1affa832643c3f7e29c3cf5f8afcfeaa62fb84f1d85"
           expected-user (with-exp {:id "3", :username "user", :role "poweruser", :auth "1"})
@@ -48,7 +48,7 @@
 
       (is (true? (:fake-called? response)))
       (is (= expected-user (:identity response)))))
-  
+
   (testing "should not allow access to resource for user with invalid role"
     (let [pw-hash               "f9480456ebc3a3259592d1affa832643c3f7e29c3cf5f8afcfeaa62fb84f1d85"
           middleware            (auth/require-roles fake-handler #{"nobody"} {3 {:password_hash pw-hash}})
@@ -61,21 +61,21 @@
 
       (is (not (true? (:fake-called? unauthorized-response))))
       (is (= {:status 403 :headers {} :body "Permission denied"} unauthorized-response))))
-  
+
   (testing "should not allow access to resource for admin user with invalid password in token"
-    (let [pw-hash1 "f9480456ebc3a3259592d1affa832643c3f7e29c3cf5f8afcfeaa62fb84f1d85"
-          pw-hash2 "014fd2c1c2ab87a484a8d9da971825d1a91d3f0f9e355315619e7f6171591000"
-          middleware (auth/require-roles fake-handler #{"admin"} {3 {:password_hash pw-hash1}})
+    (let [pw-hash1              "f9480456ebc3a3259592d1affa832643c3f7e29c3cf5f8afcfeaa62fb84f1d85"
+          pw-hash2              "014fd2c1c2ab87a484a8d9da971825d1a91d3f0f9e355315619e7f6171591000"
+          middleware            (auth/require-roles fake-handler #{"admin"} {3 {:password_hash pw-hash1}})
           unauthorized-response (middleware {:headers {"Authorization" (->auth-header (with-exp {:id       3
                                                                                                  :username "user"
                                                                                                  :role     "admin"
                                                                                                  :auth     "1"})
                                                                                       pw-hash2)}})]
       (is (= {:status 401 :headers {} :body "Unauthorized"} unauthorized-response))))
-  
+
   (testing "should not allow access to resource for admin user with invalid auth scheme in token"
-    (let [pw-hash    "f9480456ebc3a3259592d1affa832643c3f7e29c3cf5f8afcfeaa62fb84f1d85"
-          middleware (auth/require-roles fake-handler #{"admin"} {3 {:password_hash pw-hash}})
+    (let [pw-hash               "f9480456ebc3a3259592d1affa832643c3f7e29c3cf5f8afcfeaa62fb84f1d85"
+          middleware            (auth/require-roles fake-handler #{"admin"} {3 {:password_hash pw-hash}})
           unauthorized-response (middleware {:headers {"Authorization" (->auth-header (with-exp {:id       3
                                                                                                  :username "user"
                                                                                                  :role     "admin"
@@ -84,18 +84,18 @@
 
       (is (not (true? (:fake-called? unauthorized-response))))
       (is (= {:status 401 :headers {} :body "Unauthorized"} unauthorized-response))))
-  
+
   (testing "should not allow access to unauthorized user"
-    (let [pw-hash    "f9480456ebc3a3259592d1affa832643c3f7e29c3cf5f8afcfeaa62fb84f1d85"
-          middleware (auth/require-roles fake-handler #{"admin"} {3 {:password_hash pw-hash}})
+    (let [pw-hash               "f9480456ebc3a3259592d1affa832643c3f7e29c3cf5f8afcfeaa62fb84f1d85"
+          middleware            (auth/require-roles fake-handler #{"admin"} {3 {:password_hash pw-hash}})
           unauthorized-response (middleware {})]
 
       (is (not (true? (:fake-called? unauthorized-response))))
       (is (= {:status 401 :headers {} :body "Unauthorized"} unauthorized-response))))
-  
+
   (testing "should not allow access to forbidden resource"
-    (let [pw-hash    "bfebc2fee6cd4d3ecc0285e8b811c6e1c73cc9bdea8853b52256c674edf8eb16"
-          middleware (auth/require-roles fake-handler #{"admin"} {2 {:password_hash pw-hash}})
+    (let [pw-hash            "bfebc2fee6cd4d3ecc0285e8b811c6e1c73cc9bdea8853b52256c674edf8eb16"
+          middleware         (auth/require-roles fake-handler #{"admin"} {2 {:password_hash pw-hash}})
           forbidden-response (middleware {:headers {"Authorization" (->auth-header (with-exp {:id       2
                                                                                               :username "jpsilva"
                                                                                               :role     "user"
